6b150b30adaa27fde0c0af9625b72f4c236a745c
ROLEX-codes <109519996+ROLEX-codes@users.noreply.github.com>
2023-02-24 09:53:49 +0300

    Add files via upload

---
+++ function2.py
@@ -0,0 +1,8 @@
+#Volume of a cylinder
+
+def volume_of_a_cylinder():
+    r = int(input("the radius of a circle: "))
+    h = int(input("the height of a circle: "))
+    vol = 3.142*r*r*h
+    print(vol)
+volume_of_a_cylinder()
